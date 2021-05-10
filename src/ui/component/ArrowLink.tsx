460b1440b31c454084462f0228db9e5910df33ac
cgreencode <cgreencode@gmail.com>
2021-05-10 18:43:49 +0800

    browser & jest

--- src/ui/component/ArrowLink.tsx
+++ src/ui/component/ArrowLink.tsx
@@ -1,14 +1,10 @@
 import React from 'react';
-import { Link, LinkProps } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import cx from 'clsx';
 import { Icon } from 'ui/component';
 
-const ArrowLink = ({ children, className, to, ...restProps }: LinkProps) => (
-  <Link
-    className={cx('w-full flex items-center', className)}
-    to={to}
-    {...restProps}
-  >
+const ArrowLink = ({ children, className, ...restProps }) => (
+  <Link className={cx('w-full flex items-center', className)} {...restProps}>
     <div className="flex-1">{children}</div>
     <Icon type="arrow" />
   </Link>
