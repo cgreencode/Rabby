88c3d934eb82c4f81317336c649f57b1953b6fce
cgreencode <cgreencode@gmail.com>
2021-05-19 22:46:23 +0800

    feat(accounts): add watch address

--- src/background/controller/wallet.ts
+++ src/background/controller/wallet.ts
@@ -9,6 +9,7 @@ import {
   permission,
   session,
   chainService,
+  openapi,
 } from 'background/service';
 import { openIndexPage } from 'background/webapi/tab';
 import { KEYRING_CLASS, DisplayedKeryring } from 'background/service/keyring';
@@ -60,6 +61,20 @@ export class WalletController extends BaseController {
 
   clearKeyrings = () => keyringService.clearKeyrings();
 
+  importWatchAddress = async (address) => {
+    let keyring;
+    const keyringType = KEYRING_CLASS.WATCH;
+    try {
+      keyring = this._getKeyringByType(keyringType);
+    } catch {
+      keyring = await keyringService.addNewKeyring(keyringType);
+    }
+
+    keyring.setAccountToAdd(address);
+    await keyringService.addNewAccount(keyring);
+    preference.setCurrentAccount({ address, type: keyring.type });
+  };
+
   importPrivateKey = async (data) => {
     const prefixed = addHexPrefix(data);
     const buffer = ethUtil.toBuffer(prefixed);
@@ -90,8 +105,8 @@ export class WalletController extends BaseController {
   };
 
   generateMnemonic = () => keyringService.generateMnemonic();
-  importMnemonics = async (mnemonic) => {
-    const keyring = await keyringService.importMnemonics(mnemonic);
+  createKeyringWithMnemonics = async (mnemonic) => {
+    const keyring = await keyringService.createKeyringWithMnemonics(mnemonic);
     const [account] = await keyring.getAccounts();
     preference.setCurrentAccount({ address: account, type: keyring.type });
   };
@@ -124,7 +139,7 @@ export class WalletController extends BaseController {
     return seedWords;
   };
 
-  deriveNewAccount = async () => {
+  deriveNewAccountFromMnemonic = async () => {
     const keyring = this._getKeyringByType(KEYRING_CLASS.MNEMONIC);
 
     const accounts = await keyringService.addNewAccount(keyring);
@@ -235,6 +250,8 @@ export class WalletController extends BaseController {
 
     throw ethErrors.rpc.internal(`No ${type} keyring found`);
   }
+
+  openapi = openapi;
 }
 
 export default new WalletController();
